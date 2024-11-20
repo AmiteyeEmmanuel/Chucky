56ebfb6c351ee3926a593548d6d92837f1c1a1eb
Emmanuel Amiteye <emmanuelamiteye9@gmail.com>
2024-11-20 22:32:45 +0100

    fixed redux persist timeout issue

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -11,7 +11,7 @@ import { addMenu1Reducer, addMenu2Reducer, addMenuReducer } from './menu/menuSli
 const persistConfig = {
   key: 'root',
   storage,
-  timeout: 3000, 
+  timeout: 50, 
 };
 
 // Combine Reducers
