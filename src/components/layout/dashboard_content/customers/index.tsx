5041420bea00b11ebadf03f91e44ce729f2c1258
Emmanuel Amiteye <emmanuelamiteye9@gmail.com>
2024-11-21 08:35:02 +0100

    initial commits

--- src/components/layout/dashboard_content/customers/index.tsx
+++ src/components/layout/dashboard_content/customers/index.tsx
@@ -4,10 +4,10 @@ import List from "./customer/list";
 function Customer() {
   return (
     <section className="flex flex-wrap gap-4 transition-all mt-10 w-full ">
-      <div className="xl:w-[68%] w-full">
+      <div className="lg:w-[68%] w-full">
         <CustomerMap />
       </div>
-      <div className="xl:w-[30%] w-full">
+      <div className="lg:w-[30%] w-full">
         <List />
       </div>
     </section>
